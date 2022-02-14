27ebb3062a714d9e61e7f92a36ab2d5411404ff6
d400800 <d400801@gmail.com>
2022-02-14 21:05:01 +0200

    adding logger

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const logger = require('./logger');
+const {collectDefaultMetrics, register} = require('prom-client');
+
+collectDefaultMetrics();
 
 const app = express();
+
+app.get('/metrics', async (_req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
+
+app.get('/log', async (_req, res) => {
+    try {
+        logger.log({
+            level: 'info',
+            message: 'Hello distributed log files! ' + new Date()
+          });
+          
+        logger.info('Hello again distributed logs');
+
+        res.status(200).end();
+    } catch (err) {
+      res.status(500).end(err);
+    }
+  });
 
 const path = require('path');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000;
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+console.log("Starting...");
+app.listen(4001, '0.0.0.0');
